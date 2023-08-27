1a8dabd6d0d35afc2b2078304eb5fde264f92c24
hbauhvagner <hbauhvagner@gmail.com>
2023-08-27 00:13:16 -0300

    Instalando o react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
+import { BrowserRouter } from "react-router-dom";
 import Inicio from "./paginas/Inicio";
 import SobreMim from "./paginas/SobreMim";
 
-console.log(window.location);
-
-const pagina = window.location.pathname === '/' ? <Inicio /> : <SobreMim />
-
 function App() {
   return (
-    pagina
+    <BrowserRouter></BrowserRouter>
   );
 }
 
