874ccae074b4b23383b388d6f9d454da72794b23
hbauhvagner <hbauhvagner@gmail.com>
2023-09-03 02:17:25 -0300

    Utilizando rotas aninhadas

--- src/paginas/Inicio/index.js
+++ src/paginas/Inicio/index.js
@@ -1,4 +1,3 @@
-import Banner from "componentes/Banner";
 import styles from './Inicio.module.css';
 
 import posts from 'json/posts.json';
@@ -6,18 +5,14 @@ import Post from "componentes/Post";
 
 export default function Inicio() {
     return (
-        <main>
-            <Banner />
-
-            <ul className={styles.posts}>
-                {posts.map((post) => {
-                    return (
-                        <li key={post.id}>
-                            <Post post={post} />
-                        </li>
-                    )
-                })}
+        <ul className={styles.posts}>
+            {posts.map((post) => {
+                return (
+                    <li key={post.id}>
+                        <Post post={post} />
+                    </li>
+                )
+            })}
             </ul>
-        </main>
     )
 }
