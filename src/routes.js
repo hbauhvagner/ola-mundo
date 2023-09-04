50b89a7dcbe197b5080ddf2bbf50a8b7e9319579
hbauhvagner <hbauhvagner@gmail.com>
2023-09-04 00:34:25 -0300

    Implementando posts recomendados

--- src/routes.js
+++ src/routes.js
@@ -6,10 +6,12 @@ import Rodape from "componentes/Rodape";
 import PaginaPadrao from "componentes/PaginaPadrao";
 import Post from "paginas/Post";
 import NaoEncontrada from "paginas/NaoEncontrada";
+import ScrollToTop from "componentes/ScrollToTop";
 
 function AppRoutes() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Menu />
 
       <Routes>
@@ -18,7 +20,7 @@ function AppRoutes() {
           <Route path="sobremim" element={<SobreMim />} />
         </Route>
 
-        <Route path="posts/:id" element={<Post />} />
+        <Route path="posts/:id/*" element={<Post />} />
         <Route path="*" element={<NaoEncontrada />} />
       </Routes>
 
